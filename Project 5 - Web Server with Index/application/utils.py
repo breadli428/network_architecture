a0528a054d03b17aed81e8530941b126467d92a4
breadli428 <57526478+breadli428@users.noreply.github.com>
2021-02-16 12:46:20 +0100

    Delete Project 5 - Web Server with Index directory

--- Project 5 - Web Server with Index/application/utils.py
+++
@@ -1,5 +0,0 @@
-def create_http_response(status, response_body):
-    response_start_line = "HTTP/1.1 %s\r\n" % status
-    response_headers = "Server: My Server\r\n"
-    response = bytes(response_start_line + response_headers + '\r\n', 'utf-8') + response_body
-    return response
